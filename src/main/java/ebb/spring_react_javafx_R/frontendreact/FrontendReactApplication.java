6cd6dacae89f3027852e029707502cb8fa324d11
M.Sc Eduardo Barea Bermudez <ebarea1981@gmail.com>
2021-05-15 19:01:51 -0400

    First COMMIT

---
+++ src/main/java/ebb/spring_react_javafx_R/frontendreact/FrontendReactApplication.java
@@ -0,0 +1,13 @@
+package ebb.spring_react_javafx_R.frontendreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ebb/spring_react_javafx_R/frontendreact/FrontendReactApplicationTests.java
@@ -0,0 +1,13 @@
+package ebb.spring_react_javafx_R.frontendreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
