4db1ae43095085148acaeb10d9ea15d0032b05e6
M.Sc Eduardo Barea Bermudez <ebarea1981@gmail.com>
2021-05-16 00:08:19 -0400

    Adding JavaFX "browser"

--- src/main/java/ebb/spring_react_javafx_R/frontendreact/FrontendReactApplication.java
+++ src/main/java/ebb/spring_react_javafx_R/frontendreact/FrontendReactApplication.java
@@ -3,11 +3,14 @@ package ebb.spring_react_javafx_R.frontendreact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ebb.spring_react_javafx_R.browser.BrowserApp;
+
 @SpringBootApplication
 public class FrontendReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendReactApplication.class, args);
+		BrowserApp.main(args);
 	}
 
 }
